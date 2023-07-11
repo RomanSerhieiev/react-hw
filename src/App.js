f00bcdbd5606f42ea94489762abcb1b432339733
RomanSerhieiev <unique.rmn.8@gmail.com>
2023-07-11 11:21:36 +0300

    hw-3 1.1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-function App() {
-  return (
-    <div>
+import { useEffect, useState } from 'react';
 
-    </div>
-  );
+const App = () => {
+    const [users, setUsers] = useState(null);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((res) => res.json())
+            .then((users) => setUsers(users));
+    }, []);
+
+    return (
+        <>
+            <h1>Homework 3</h1>
+
+        </>
+    );
 }
 
 export default App;
---
+++ src/PropertyDrill/Users.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Users = () => {
+    return (
+        <div>
+            <h2>Property Drill</h2>
+
+        </div>
+    );
+};
+
+export default Users;
